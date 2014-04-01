d1fc0315f54625729bd6a2ce13243df9d440707e
Matt Mower <mowerm@gmail.com>
2014-04-01 17:49:52 -0500

    Override product name
    Change-Id: I9c34f64b94cd6f423bb16578d2e9b2edec1e922c

--- cm.mk
+++ cm.mk
@@ -5,11 +5,14 @@ PRODUCT_RELEASE_NAME := evita
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/htc/evita/device_evita.mk)
+$(call inherit-product, device/htc/evita/device.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := evita
 PRODUCT_NAME := cm_evita
 PRODUCT_BRAND := htc
-PRODUCT_MODEL := evita
-PRODUCT_MANUFACTURER := htc
+PRODUCT_MODEL := One X
+PRODUCT_MANUFACTURER := HTC
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=htc_evita
